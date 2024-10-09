b761fb04658eaa306705612bc7d8669b47a1c43f
Şevval Taner <77547613+Sevvaltaner@users.noreply.github.com>
2024-10-09 12:20:04 +0300

    getAndRemove addTile and isWinningHand methods are implemented

--- Player.java
+++ Player.java
@@ -13,26 +13,70 @@ public class Player {
      * TODO: removes and returns the tile in given index
      */
     public Tile getAndRemoveTile(int index) {
-        return null;
+        Tile removedTile = playerTiles[index];
+        // Shifts the rest of the tiles to fill the gap
+        for (int i = index; i < numberOfTiles - 1; i++) {
+            playerTiles[i] = playerTiles[i + 1];
+        }
+        playerTiles[numberOfTiles - 1] = null; // Remove the last tile (now duplicate)
+        numberOfTiles--; // Decrease tile count
+        return removedTile;
     }
 
     /*
-     * TODO: adds the given tile to the playerTiles in order
+     * adds the given tile to the playerTiles in order
      * should also update numberOfTiles accordingly.
      * make sure playerTiles are not more than 15 at any time
      */
     public void addTile(Tile t) {
+        if (numberOfTiles >= 15) {
+            System.out.println("Cannot add more tiles. Maximum limit reached.");
+            return;
+        }
+        // Find the correct position to insert the new tile while keeping the array sorted
+        int indexNewTile = numberOfTiles;
+        for (int i = 0; i < numberOfTiles; i++) {
+            if (t.compareTo(playerTiles[i]) < 0) {
+                indexNewTile = i;
+                break;
+            }
+        }
+    
+        // Shift elements to the right to make a room for the new tile
+        for (int i = numberOfTiles; i > indexNewTile; i--) {
+            playerTiles[i] = playerTiles[i - 1];
+        }
+    
+        playerTiles[indexNewTile] = t;
+        numberOfTiles++;
 
     }
 
     /*
-     * TODO: checks if this player's hand satisfies the winning condition
+     * checks if this player's hand satisfies the winning condition
      * to win this player should have 3 chains of length 4, extra tiles
      * does not disturb the winning condition
-     * @return
      */
     public boolean isWinningHand() {
-        return false;
+        int chainsOfFour = 0;
+        int chainMember = 1; // length of the chain
+    
+        for (int i = 1; i < numberOfTiles; i++) {
+            // Check if the current tile can form a chain with the previous one
+            if (playerTiles[i].canFormChainWith(playerTiles[i - 1])) {
+                chainMember++;
+            } else {
+                chainMember = 1; // Reset chain count
+            }
+    
+            // If we have a chain of 4, count it
+            if (chainMember == 4) {
+                chainsOfFour++;
+                chainMember = 1; // Reset to search for the next chain
+            }
+        }
+    
+        return chainsOfFour >= 3;
     }
 
     public int findPositionOfTile(Tile t) {
